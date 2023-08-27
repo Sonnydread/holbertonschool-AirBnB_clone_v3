92033c0abe082c881ed39b5cfb3d15123e930399
Sonnydread <alvaradoguille590@gmail.com>
2023-08-27 16:09:23 -0700

    addingapi

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Flask web application
-"""
+"""Flask web application"""
+
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
