0494d0d4e09ccb46456efc9e23ab58e82de5259c
Sonnydread <alvaradoguille590@gmail.com>
2023-08-27 14:57:39 -0700

    apiversion

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask web application"""
+"""first endpoint (route) will be to return the status"""
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
