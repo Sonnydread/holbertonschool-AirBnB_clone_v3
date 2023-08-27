27997498ea07362e0cc6d3a458863bbf2fa36490
Sonnydread <alvaradoguille590@gmail.com>
2023-08-27 15:28:37 -0700

    solvingprob

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Flask web application"""
+"""
+Flask web application
+"""
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
